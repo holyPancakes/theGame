1d2c3f56cfbd7edc2dc673adea06d6516e8e1702
asvibar <asvibar@up.edu.ph>
2016-05-13 04:33:49 +0800

    Finished Intro

--- ics-os/contrib/theGameSeries/thegame.c
+++ ics-os/contrib/theGameSeries/thegame.c
@@ -31,7 +31,8 @@ void playGame();
 void credits();
 void settings();
 void quit();
-
+void menu();
+void playIntro();
 
 int main() {
 	int x_menu=0,y_menu=0;
@@ -101,12 +102,12 @@ int main() {
 void writeTextAnimation(char* text, int x, int y, int color, int maxWidth, int size){
 	char sub[256];
 	int i;
-	for ( i = 0 ; i < strlen(text) ; i++) {
+	for ( i = 0 ; i < strlen(text)+1 ; i++) {
 		memset( sub, '\0', sizeof(sub) );
 		memcpy( sub, &text[(i/maxWidth)*maxWidth], i%maxWidth );
 		sub[strlen(text)%maxWidth] = '\0';
-		write_text(sub,45,50+(i/maxWidth)*10,WHITE,size);
-		delay(5);
+		write_text(sub,x,y+(i/maxWidth)*10,WHITE,size);
+		delay(3);
 		//if(i%maxWidth == (maxWidth-1)){
         //    write_text(">",45+maxWidth, 80 , WHITE, 0);
         //    getch();
@@ -117,7 +118,8 @@ void writeTextAnimation(char* text, int x, int y, int color, int maxWidth, int s
 
 void playGame(){
 	clearArea(0,0,440,220);
-	writeTextAnimation("Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.", 20, 5, WHITE, 30, 0);
+	playIntro();
+	//writeTextAnimation("Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.", 20, 5, WHITE, 30, 0);
 	//Insert Intro here
 }
 
@@ -140,4 +142,42 @@ void clearArea(int x, int y, int w, int h) {
          write_pixel(j,i,0x0000);
       }
    }
+}
+
+void playIntro(){
+    char* title = "THE GAME \n PROJECT";
+
+    writeTextAnimation("One day, in the year XX16",30,45,WHITE,29,0);
+    delay(5);
+    writeTextAnimation("In a galaxy not really far away",30,55,WHITE,40,0);
+    clearArea(0,0,440,220);
+    writeTextAnimation("You'll finally finish",30,45,WHITE,49,0);
+	delay(5);
+    writeTextAnimation("Your game project in CMSC 125",30,55,WHITE,30,0);
+    delay(5);
+    clearArea(0,0,440,220);
+    writeTextAnimation("But then a portal opened",30,55,WHITE,30,0);
+    delay(5);
+    clearArea(0,0,440,220);
+    writeTextAnimation("And you got into a world",30,45,WHITE,35,0);
+    delay(5);
+    writeTextAnimation("where random people show up",30,55,WHITE,30,0);
+    delay(5);
+    clearArea(0,0,440,220);
+    writeTextAnimation("You must now go home",30,45,WHITE,35,0);
+    delay(5);
+    writeTextAnimation("with the help of Jackie Chan",30,55,WHITE,30,0);
+    delay(5);
+    clearArea(0,0,440,220);
+    writeTextAnimation("And finish...",30,45,WHITE,29,0);
+    delay(5);
+    clearArea(0,0,440,220);
+    delay(5);
+    writeTextAnimation(title,40,50,WHITE,28,1);
+
+    //write_text(".the Game",45,130,DARK_BLUE,1);
+	write_text("Play",40,150,WHITE,0);
+	write_text("Settings",40,170,WHITE,0);
+	write_text("Credits",150,150,WHITE,0);
+	write_text("Quit",150,170,WHITE,0);
 }
