a3413a3fb1bc4e2c16ce030733046998cf8acf38
holyPancakes <marubaul22@gmail.com>
2016-05-12 15:28:36 +0800

    Here's the message you want bitch

--- ics-os/contrib/theGameSeries/thegame.c
+++ ics-os/contrib/theGameSeries/thegame.c
@@ -2,14 +2,12 @@
 #include "../../sdk/dexsdk.h"
 #include "../../sdk/time.h"
 
-
 #define okay 'z'
 #define back 'x'
 #define up_key (char)151
 #define down_key (char)152
 #define left_key (char)150
 #define right_key (char)153
-
 
 #define SKY_BLUE 3
 #define GREEN 2
@@ -24,18 +22,23 @@
 #define NORMAL 5
 #define FAST 3
 
-
 int row, col;
 int flip;
 
+void writeTextAnimation(char* text, int x, int y, int color, int maxWidth, int size);
 void clearArea(int x, int y, int w, int h);
+void playGame();
+void credits();
+void settings();
+void quit();
+
 
 int main() {
 	int x_menu=0,y_menu=0;
 	char keypress;
 	int i=0, textSpeed = NORMAL;
 	set_graphics(VGA_320X200X256);
-	//erase(1,1,770,220);
+	clrscr();
 
 	write_text(".the Game",45,130,DARK_BLUE,1);
 	write_text("Play",40,150,WHITE,0);
@@ -43,23 +46,15 @@ int main() {
 	write_text("Credits",150,150,WHITE,0);
 	write_text("Quit",150,170,WHITE,0);
 	write_text(">",30+x_menu*110,150+y_menu*20,WHITE,0);
-	//for (i = 0; i < 256; i++) {
-	//	write_text("Y",i*4,130,i,9);
-	//
-	//}
+
 	do{
 		keypress = (char)getch();
-		char* text = "In the year 2016, a man named _____";
-		char sub[256];
-		for ( i = 0 ; i < strlen(text) ; i++) {
-			memcpy( sub, &text[0], i );
-			sub[strlen(text)] = '\0';
-			write_text(sub,45,100,WHITE,1);
-			delay(5);
-		}
+		//char* text = "In the year 2016, a man named _____";
+
 
 		clearArea(30,150,5,30);
 		clearArea(140,150,5,30);
+
 		if(keypress==up_key){
 			i = (i+1)%63;
 			if(y_menu==1){
@@ -82,6 +77,18 @@ int main() {
 				x_menu=1;
 			}
 		}
+		else if(keypress==okay && x_menu==0 && y_menu==0 ){
+			playGame();
+		}
+		else if(keypress==okay && x_menu==0 && y_menu==1 ){
+			credits();
+		}
+		else if(keypress==okay && x_menu==0 && y_menu==0 ){
+			settings();
+		}
+		else if(keypress==okay && x_menu==0 && y_menu==0 ){
+			quit();
+		}
 		write_text(">",30+x_menu*110,150+y_menu*20,WHITE,0);
 	}while(keypress!='q');
 
@@ -91,8 +98,41 @@ int main() {
 	return 0;
 }
 
-void writeText(char* text, , , ,){
+void writeTextAnimation(char* text, int x, int y, int color, int maxWidth, int size){
+	char sub[256];
+	int i;
+	for ( i = 0 ; i < strlen(text) ; i++) {
+		memset( sub, '\0', sizeof(sub) );
+		memcpy( sub, &text[(i/maxWidth)*maxWidth], i%maxWidth );
+		sub[strlen(text)%maxWidth] = '\0';
+		write_text(sub,45,50+(i/maxWidth)*10,WHITE,size);
+		delay(5);
+		if(i%maxWidth){
 
+		}
+	}
+}
+
+void playGame(){
+	clrscr();
+	
+	writeTextAnimation("Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.", 20, 5, WHITE, 30, 0);
+	//Insert Intro here
+}
+
+void credits(){
+	clrscr();
+	//Insert Intro here
+}
+
+void settings(){
+	clrscr();
+	//Insert Intro here
+}
+
+void quit(){
+	clrscr();
+	//Insert Intro here
 }
 
 void clearArea(int x, int y, int w, int h) {
